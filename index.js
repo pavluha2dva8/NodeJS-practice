6e91b384b36381c40626fd652ab2122f6d8d72b9
pavluha2dva8 <pavluha2dva8@gmail.com>
2021-02-12 15:50:37 +0200

    alrignt

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-console.log(__filename)
-console.log(__dirname)
+/*
+let things = require('./things')
+let {someValue} = require('./things')
+
+console.log(someValue)
+console.log(things.multiply(5, 10))
+console.log(things.arrayCounter([2, 1, 3, 4, 5]))
+*/
+// =========================================================
+/*
+let events = require('events')
+
+let myEmit = new events.EventEmitter()
+
+myEmit.on('some_event', function(text) {
+    console.log(text)
+})
+
+myEmit.emit('some_event', 'event start')
+*/
+// =========================================================
+let events = require('events')
+let util = require('util')
+
+class Cars {
+    constructor(model) {
+        this.model = model
+    }
+}
+
+util.inherits(Cars, events.EventEmitter)
+
+let bmw = new Cars('БНВ')
+let audi = new Cars('Audi')
+let volvo = new Cars('Volvo')
+
+let cars =[bmw, audi, volvo]
+
+cars.forEach( (car) => {
+    car.on('speed', (text) => {
+        console.log(`${car.model} speed is - ${text}`)
+    })
+})
+
+bmw.emit('speed', '184.5 km/h')
+audi.emit('speed', '284.5 km/h')
+volvo.emit('speed', '84.5 km/h')
---
+++ things.js
@@ -0,0 +1,21 @@
+// let arrayCounter = function(arr) {
+//     return `Array length ${arr.length}`
+// }
+
+// module.exports.multiply = function(x, y) {
+//     return `${x} * ${y} = ${x * y}`
+// } 
+
+// module.exports.someValue = 228
+
+let arrayCounter = function(arr) {
+    return `Array length ${arr.length}`
+}
+
+let multiply = function(x, y) {
+    return `${x} * ${y} = ${x * y}`
+}
+
+let someValue = 228
+
+module.exports = { arrayCounter, multiply, someValue }
