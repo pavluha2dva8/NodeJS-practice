08315199c76e2af3dd9af633fe15c8696289871f
pavluha2dva8 <pavluha2dva8@gmail.com>
2021-02-16 15:41:45 +0200

    new vid

--- index.js
+++ index.js
@@ -55,3 +55,25 @@ server.listen(3000, '127.0.0.1')
 console.log('Local server started')
 */
 // =========================================================
+// Маршрутизація (видача сторінки відносно URL)
+
+let fs = require('fs')
+// Підключення до локального сервера
+let http = require('http')
+
+let server = http.createServer((req, res) => {
+    console.log('Page URL: ' + req.url)
+    if (req.url === '/index' || req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+        fs.createReadStream(__dirname + '/about.html').pipe(res)
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' })
+        fs.createReadStream(__dirname + '/404.html').pipe(res)
+    }
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('Ми слідкуємо за портом 3000')
